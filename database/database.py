38cf692c11921f4a1f6e3396424273358d4cee9e
lunkov <89242194144@mail.ru>
2023-11-21 20:34:02 +1000

    The database is connected 2.

--- database/database.py
+++ database/database.py
@@ -1,15 +1,17 @@
 import psycopg2
-from config import host, user, password, db_name, port
+from config import HOST, USER, PASSWORD, DB_NAME, PORT
 
 try:
     # connect to exist database
     conn = psycopg2.connect(
-        host=host,
-        user=user,
-        password=password,
-        database=db_name,
-        port=port
+        host=HOST,
+        user=USER,
+        password=PASSWORD,
+        database=DB_NAME,
+        port=PORT
     )
+    
+    conn.autocommit = True
 
     # the cursor for perfoming database operations
     with conn.cursor() as cursor:
