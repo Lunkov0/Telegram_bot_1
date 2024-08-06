9fc94469baabee91e740cabd41cbf9236d44810e
Vladimir Lunkov <89242194144@mail.ru>
2024-08-07 00:10:28 +1000

    added tests for merge_time function

--- Telegram_bot_1/tests/test_make_appointment.py
+++ Telegram_bot_1/tests/test_make_appointment.py
@@ -1,14 +1,20 @@
 import unittest
 from datetime import time
+from handlers.make_appointment import merge_time
 
 
-class TestAppointmentTime(unittest.TestCase):
+class TestMergeTime(unittest.TestCase):
+    def test_inside(self):
+        main = [[time(9), time(18)]]
+        changes = [time(13), time(14)]
+        res = [[time(9), time(13)], [time(14), time(18)]]
+        self.assertEqual(merge_time(main, changes), res)
 
-    def test_appointment_tme(self):
-        constant_breaks = [time(12), time(13)]
-        main_schedule = {3: [[time(9), time(16)]]}
-        res = {3: [[time(9), time(12)], [time(13), time(16)]]}
-        self.assertEqual(merge_main_and_schedule_break(main_schedule, constant_breaks), res)
+    def test_outside(self):
+        main = [[time(13), time(14)]]
+        changes = [time(9), time(18)]
+        res = []
+        self.assertEqual(merge_time(main, changes), res)
 
 
 if __name__ == '__main__':
