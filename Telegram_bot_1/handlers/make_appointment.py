fdc3151d9ceecc782c8156a7cfc789e3006b8e73
Vladimir Lunkov <89242194144@mail.ru>
2024-09-01 18:41:27 +1000

    added schedule_make_appointment_fsm handler

--- Telegram_bot_1/handlers/make_appointment.py
+++ Telegram_bot_1/handlers/make_appointment.py
@@ -1,14 +1,10 @@
 from aiogram import Router, F
-from aiogram.filters import Command
 from aiogram import types
-from aiogram.utils.keyboard import InlineKeyboardBuilder
 from aiogram.fsm.context import FSMContext
-import datetime
 import locale
 
-# from keyboards.kStart import
 from database.database import dataBase
-from functions import list_to_keyboard, time_to_str, appointment_time, treatment_schedule
+from functions import list_to_keyboard, treatment_schedule, str_to_date, date_to_str
 from utils.states import MakeAppointmentFSM
 
 # Установка русской локализации для модуля datetime
@@ -18,66 +14,6 @@ locale.setlocale(
 )
 
 router = Router()
-
-
-def str_to_date(str_date):
-    return datetime.datetime.strptime(str_date, '%Y-%m-%d').date()
-
-
-def date_to_str(date):
-    return date.strftime('%Y-%m-%d')
-
-
-def merge_time(main: list[list[datetime]],
-               second: list[datetime],
-               is_it_working_day: int = 0) -> list[list[datetime]]:
-    if not second:
-        return main
-
-    res = []
-    start2, end2 = second[0], second[1]
-
-    if is_it_working_day == 0:
-        for start1, end1 in sorted(main):
-            if start1 <= start2 and end1 >= end2:
-                res.append([start1, start2])
-                res.append([end2, end1])
-            elif start1 >= start2 and end1 >= end2:
-                res.append([max(start1, end2), end1])
-            elif start1 <= start2 and end1 <= end2:
-                res.append([start1, min(end1, start2)])
-
-    elif is_it_working_day == 1:
-        sec = [second[0], second[1]]
-        for start1, end1 in sorted(main):
-            if start1 > end2 or end1 < start2:
-                res.append([start1, end1])
-
-            elif start1 <= start2 and end1 >= end2:
-                res.append([start1, end1])
-                start2, end2 = start1, end1
-                sec = []
-
-            else:
-                start2 = min(start1, start2)
-                end2 = max(end1, end2)
-                sec = [start2, end2]
-
-        if sec:
-            res.append(sec)
-
-    return sorted(res)
-
-
-def merge_many(main: list[list[datetime]],
-               second: list[list[datetime]],
-               is_it_working_day: int = 0) -> list[list[datetime]]:
-    res = []
-
-    for val in second:
-        res.extend(merge_time(main, val, is_it_working_day))
-
-    return res
 
 
 @router.callback_query(F.data == 'make_an_appointment')
@@ -93,25 +29,33 @@ async def make_an_appointment(callback: types.CallbackQuery, state: FSMContext):
 async def m_a_treatment(callback: types.CallbackQuery, state: FSMContext):
     treatment = callback.data
     await state.update_data(treatment=treatment)
-    await state.set_state(MakeAppointmentFSM.schedule)
 
     schedule = treatment_schedule(treatment)
     await state.update_data(schedule=schedule)
 
-    keyboard = list_to_keyboard([str(day) for day in schedule if schedule[day]])
-    # keyboard.add(types.InlineKeyboardButton(text='<< Отмена', callback_data='<< Отмена'))
+    keyboard = list_to_keyboard([str(day) for day in schedule if schedule[day]], 3)
 
     txt = 'Вы выбрали процедуру: ' + treatment + ('.\nНиже показаны даты где есть свободное время.'
                                                   ' Нажмите, чтобы увидеть сободные часы для записи.')
 
+    await state.set_state(MakeAppointmentFSM.schedule)
     await callback.message.answer(text=txt, reply_markup=keyboard)
 
 
-# @router.message(MakeAppointmentFSM.schedule)
-# async def schedule_make_appointment_fsm(callback: types.CallbackQuery, state: FSMContext):
-#     start_time = message.text
-#     await state.update_data(start_time=start_time)
-#     await message.answer(text=start_time)
+@router.callback_query(MakeAppointmentFSM.schedule)
+async def schedule_make_appointment_fsm(callback: types.CallbackQuery, state: FSMContext):
+    day = callback.data
+    schedule = await state.get_data()
+    schedule = schedule.get('schedule')
+
+    txt = f'Вы выбрали дату: {day}, ниже показано свободное для записи время на этот день.'
+
+    day = str_to_date(day)
+    await state.update_data(day=day)
+
+    keyboard = list_to_keyboard(schedule[day])
+
+    await callback.message.answer(text=txt, reply_markup=keyboard)
 
 
 @router.message(F.text == '333')
@@ -119,4 +63,3 @@ async def m_a_treatment(callback: types.CallbackQuery, state: FSMContext):
 async def some(message: types.Message):
     keyboard = list_to_keyboard(['1', '2', '3', '4', '5', '6', '3', '3', '41', '2', '5'], 3)
     await message.answer(text='Some активирован!!!', reply_markup=keyboard)
-
